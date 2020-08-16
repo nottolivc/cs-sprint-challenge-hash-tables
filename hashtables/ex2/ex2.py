6d7d53e0f2388fd33cfdca0b7c8963f98d8d3d05
Capo Nottoli <andrewnottoli@users-iMac.local>
2020-08-16 16:11:44 -0700

    setting up repo, question 2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,18 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    trip = {}
+    route = []
+
+    for ticket in tickets:
+        trip[ticket.source] = ticket.destination
+    
+    next_trip = trip['NONE']
+
+    while next_trip != 'NONE':
+        route.append(next_trip)
+        next_trip = trip[next_trip]
+
+    route.append('NONE')
 
     return route
